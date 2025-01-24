1be48cfe5aa45f04955f1bcd1a750cbdde5fab2a
Aian Kharitonov <kharitonovayan2018@gmail.com>
2025-01-23 23:08:21 -0500

    editing main.py and importing mask_account_card from src.widget.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card  # Импорт из src/widget.py
 
 if __name__ == "__main__":
     print(get_mask_account(7000792289606361))
     print(get_mask_card_number(7000792289606361))
+    print(mask_account_card("Visa Platinum 7000792289606361"))
+    print(mask_account_card("Счет 35383033474447895560"))
+
+
+
+
