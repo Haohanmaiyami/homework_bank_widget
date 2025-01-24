cb64bab3ce4de39b04f06faf0a5203be4804be7b
Aian Kharitonov <kharitonovayan2018@gmail.com>
2025-01-23 23:33:07 -0500

    add final function with the date

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from src.masks import get_mask_account, get_mask_card_number
 from src.widget import mask_account_card  # Импорт из src/widget.py
+from src.widget import get_date
 
 if __name__ == "__main__":
     print(get_mask_account(7000792289606361))
     print(get_mask_card_number(7000792289606361))
     print(mask_account_card("Visa Platinum 7000792289606361"))
-    print(mask_account_card("Счет 35383033474447895560"))
-
+    print(mask_account_card("Maestro 7000792289606361"))
+    print(mask_account_card("Счет 73654108430135874305"))
+    date_input = "2018-07-11T02:26:18.671407"
+    formatted_date = get_date(date_input)
+    print(formatted_date)  # Ожидаемый результат: "11.03.2024"
 
 
 
--- src/widget.py
+++ src/widget.py
@@ -16,3 +16,14 @@ def mask_account_card(info: str) -> str:
         card_number = parts[1]
         masked_card = get_mask_card_number(int(card_number))
         return f"{card_type} {masked_card}"
+
+from datetime import datetime
+
+def get_date(date_str: str) -> str:
+    """
+    Преобразует строку с датой из формата ISO 8601 в формат ДД.ММ.ГГГГ.
+    """
+    # Преобразуем строку в объект datetime
+    dt = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
+    # Возвращаем строку в формате "ДД.ММ.ГГГГ"
+    return dt.strftime("%d.%m.%Y")
