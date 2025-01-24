1efd0dda107c0980f8886d53352f39ab6a83729d
Aian Kharitonov <kharitonovayan2018@gmail.com>
2025-01-23 22:57:27 -0500

    add widget and function in it

---
+++ src/widget.py
@@ -0,0 +1,18 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+def mask_account_card(info: str) -> str:
+    """
+    Маскирует номер карты или счёта в зависимости от входной строки.
+    """
+    if info.startswith("Счет"):
+        # Работа с номером счёта
+        account_number = info.split()[-1]  # Извлекаем номер счёта
+        masked_account = get_mask_account(int(account_number))
+        return f"Счет {masked_account}"
+    else:
+        # Работа с номером карты
+        parts = info.rsplit(" ", 1)  # Разделяем строку на тип карты и номер
+        card_type = parts[0]
+        card_number = parts[1]
+        masked_card = get_mask_card_number(int(card_number))
+        return f"{card_type} {masked_card}"
