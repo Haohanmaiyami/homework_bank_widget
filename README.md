bea5084b0c6b3dc1ffbd2b1e69deb9ee0cf597b2
Aian Kharitonov <kharitonovayan2018@gmail.com>
2025-02-01 17:31:52 -0500

    fixes before sending

---
+++ README.md
@@ -0,0 +1,7 @@
+# Data processing project
+## Project description 
+Project designed for data processing and 
+includes masking info regarding personal data
+filtering and sorting functions. The project is 
+developed using GitFlow and hosted on GitHub 
+for collaboration. 
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from src.masks import get_mask_account, get_mask_card_number
 from src.widget import mask_account_card  # Импорт из src/widget.py
 from src.widget import get_date
+from src.processing import filter_by_state, sort_by_date
+
+
+if __name__ == '__main__':
+    transactions = [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ]
+# Определяем переменные sorted_desc и sorted_asc
+    sorted_desc = sort_by_date(transactions)  # Сортировка по убыванию
+    sorted_asc = sort_by_date(transactions, descending=False)  # Сортировка по возрастанию
+
 
 if __name__ == "__main__":
     print(get_mask_account(7000792289606361))
@@ -11,3 +25,7 @@ if __name__ == "__main__":
     date_input = "2018-07-11T02:26:18.671407"
     formatted_date = get_date(date_input)
     print(formatted_date)  # Ожидаемый результат: "11.07.2018"
+    print(filter_by_state(transactions,  "EXECUTED"))
+    print(filter_by_state(transactions,  "CANCELED"))
+    print("Сортировка по убыванию:", sorted_desc)
+    print("Сортировка по возрастанию:", sorted_asc)
