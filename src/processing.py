86ce74ff82e9392e2bf2d5fc04fd455159ea0f65
Aian Kharitonov <kharitonovayan2018@gmail.com>
2025-02-01 18:34:33 -0500

    fixes

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,12 @@
+from typing import Dict, List
+
+
 def filter_by_state(transactions: list[dict], state: str = "EXECUTED") -> list[dict]:
     """ Функция фильтрует транзакции"""
     return [transaction for transaction in transactions if transaction["state"] == state]
 
 
-def sort_by_date(data, descending=True):
+def sort_by_date(data: List[Dict], descending: bool = True) -> List[Dict]:
     """
     Сортирует список словарей по ключу 'date'.
     descending - порядок сортировки (по умолчанию True - по убыванию)
