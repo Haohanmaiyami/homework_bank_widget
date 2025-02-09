a1898d0ee65e4bcbbb62fd9d3a91acfbdda7344a
Aian Kharitonov <kharitonovayan2018@gmail.com>
2025-02-09 02:57:24 -0500

    homework 10.2 all fixes

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ from src.processing import filter_by_state, sort_by_date
 from src.widget import mask_account_card  # Импорт из src/widget.py
 from src.widget import get_date
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     transactions = [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
-# Определяем переменные sorted_desc и sorted_asc
+    # Определяем переменные sorted_desc и sorted_asc
     sorted_desc = sort_by_date(transactions)  # Сортировка по убыванию
-    sorted_asc = sort_by_date(transactions, descending=False)  # Сортировка по возрастанию
+    sorted_asc = sort_by_date(
+        transactions, descending=False
+    )  # Сортировка по возрастанию
 
 
 if __name__ == "__main__":
---
+++ tests/test_processing.py
@@ -0,0 +1,14 @@
+
+from src.processing import filter_by_state, sort_by_date
+
+
+def test_filter_by_state(sample_transactions):
+    result = filter_by_state(sample_transactions, state="EXECUTED")
+    assert len(result) == 2
+    assert all(tx["state"] == "EXECUTED" for tx in result)
+
+
+def test_sort_by_date(sample_transactions):
+    result = sort_by_date(sample_transactions)
+    assert result[0]["date"] == "2025-02-05"
+    assert result[-1]["date"] == "2025-01-01"
